6fd9688354bfab6cda03eb7cc85f458d5e97619e
Ramon Barros <ramondesbarros@gmail.com>
2024-09-14 23:53:09 -0300

    init project

---
+++ src/test/java/com/pagamentosimplificado/PagamentosimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagamentosimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
